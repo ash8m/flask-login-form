eac446fe2fa71c3e716b13426b964f0c7e9400e2
ash8m <ashishtom674@gmail.com>
2024-01-13 00:02:33 +0000

    changes to init.py for docker compose

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,34 @@
+import time
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.exc import OperationalError
 
 db=SQLAlchemy()
-DB_NAME = "users.db"
+DB_NAME = "users"
+
+def wait_for_db(db_uri):
+    db_engine = create_engine(db_uri)
+    while True:
+        try:
+            connection = db_engine.connect()
+            connection.close()
+            break
+        except OperationalError:
+            print("Database not ready yet. Waiting...")
+            time.sleep(10)
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "adsfjwuenvcjWEOPoiajfjsndsiuhwe"
+    app.config["SECRET_KEY"] = "adsfjwuenvcjWEOPoiajfjsndsiuhwe" 
     app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://root:password@sql-db:3306/{DB_NAME}'
+    wait_for_db(app.config["SQLALCHEMY_DATABASE_URI"])
     db.init_app(app)
-    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-        create_database(app.config["SQLALCHEMY_DATABASE_URI"])
-        print('Created Database!')
+    #if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+    #    create_database(app.config["SQLALCHEMY_DATABASE_URI"])
+    #    print('Created Database!')
 
 
     from .views import views
